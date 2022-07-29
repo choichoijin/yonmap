d6b9d674d1166ea04951cfddf078051d0f08f908
choichoijin <cuj10594@yonsei.ac.kr>
2022-07-30 01:34:40 +0900

    feat: 마커 이미지 생성

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -8,8 +8,8 @@ function Home() {
       <SideBar>
         <StLogo>YON MAP</StLogo>
         <StSearchContainer>
-          <StBeforeClassInput></StBeforeClassInput>
-          <StAfterClassInput></StAfterClassInput>
+          <StBeforeClassInput placeholder="앞 강의 입력"></StBeforeClassInput>
+          <StAfterClassInput placeholder="뒷 강의 입력"></StAfterClassInput>
         </StSearchContainer>
         <StSearchButton>
           <button>길찾기</button>
@@ -36,12 +36,27 @@ const StLogo = styled.h1`
   font-size: 2.3rem;
   font-weight: 700;
   color: #647dff;
+  text-align: center;
+  margin: 4.5rem 0 1.6rem 0;
 `;
 
-const StSearchContainer = styled.div``;
+const StSearchContainer = styled.div`
+  & > input {
+    width: 21rem;
+    height: 3.5rem;
+    border-radius: 2.9rem;
+    border-style: none;
+    margin: 1.5rem 3rem 0 3rem;
+    padding-left: 2rem;
+  }
+`;
 
 const StSearchButton = styled.div`
   & > button {
+    width: 21rem;
+    height: 3.5rem;
+    border-radius: 2.9rem;
+    margin: 1.5rem 3rem 0 3rem;
     background-color: #c1c8ff;
     border-radius: 2.9rem;
     color: white;
--- src/home/Map.tsx
+++ src/home/Map.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { markAsUntransferable } from "worker_threads";
 
 // interface MapProps {
 //   latitude: number;
@@ -14,11 +15,30 @@ const Map = () => {
 
     const options = {
       //지도를 생성할 때 필요한 기본 옵션
-      center: new window.kakao.maps.LatLng(37.56418, 126.93838), //지도의 중심좌표.
+      center: new window.kakao.maps.LatLng(37.563, 126.9383), //지도의 중심좌표.
       level: 3, //지도의 레벨(확대, 축소 정도)
     };
 
     const map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+    //마커 이미지 정보
+    const imageSrc = "https://i.ibb.co/5nksNGp/image.png";
+    const imageSize = new window.kakao.maps.Size(28, 40);
+    const imageOption = { offset: new window.kakao.maps.Point(27, 69) };
+
+    const markerImage = new window.kakao.maps.MarkerImage(
+      imageSrc,
+      imageSize,
+      imageOption
+    );
+    const markerPosition = new window.kakao.maps.LatLng(37.563, 126.9383);
+
+    const marker = new window.kakao.maps.Marker({
+      position: markerPosition,
+      image: markerImage,
+    });
+
+    marker.setMap(map);
   }, []);
 
   return <MapContainer id="map" />;
